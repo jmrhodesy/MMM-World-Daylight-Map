7c8698d72f0dcd2c82b61ab563d08968507e3ae2
Jonathan <jmrhodesy@gmail.com>
2020-12-20 15:40:37 -0800

    Adding MMM files

---
+++ MMM-World-Daylight-Map.js
@@ -0,0 +1,58 @@
+/* global cloneObject */
+
+/* Magic Mirror
+ * Module: World Daylight Map
+ *
+ * By Jonathan Rhodes
+ * MIT Licensed.
+ */
+
+
+Module.register("MMM-World-Daylight-Map", {
+
+    defaults: {
+        fetchInterval: 5 * 60 * 1000 // Update every 5 minutes.
+    },
+
+    getScripts: function() {
+        return [
+            "d3.min.js", 
+            "jquery.min.js", 
+            "topojson.min.js",
+            "d3.geo.projection.min.js", 
+            "moment.min.js", 
+            "moment-timezone.min.js", 
+            "suncalc.js", 
+            "script.js"
+        ]
+    },
+
+    start: function (){
+        this.count = 0
+        var timer = setInterval(()=>{
+          this.updateDom()
+        }, this.config.fetchInterval)
+      },
+
+      getDom: function() {
+        
+        var element = document.createElement("div")
+        element.innerHTML = "<h1>World Daylight Map</h1>"
+        + "<div class=\"container\">"
+        + "<h4>This map is cool!</h4>"
+        + "<svg id=\"daylight-map\"></svg>"
+        + "</div>"
+        + "<script src='./d3.min.js'></script>"
+        + "<script src='./jquery.min.js'></script>"
+        + "<script src='./topojson.min.js'></script>"
+        + "<script src='./d3.geo.projection.min.js'></script>"
+        + "<script src='./moment.min.js'></script>"
+        + "<script src='./moment-timezone.min.js'></script>"
+        + "<script src='./suncalc.js'></script>"
+        + "<script src=\"./script.js\"></script>"
+
+        return element
+      }
+  })
+
+  
